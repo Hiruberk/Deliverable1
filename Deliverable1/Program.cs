e1d2bf16af0e647e4b2f22923bb49d6c4b3580e8
Nate <nsdavis@mtu.edu>
2020-12-12 16:28:54 -0500

    Add project files.

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Defining the user's input as a string variable to check later and a boolean variable for later.
+        string password = password;
+        bool valid = false;
+          //Intializes the loop to iterate until it meets requirements.
+          while (!valid) {
+          // Describes requirements and asks for input.
+            Console.WriteLine("Please enter a Password: \n\nThe password must contain the following requirements:\nAt least one lowercase letter\nAt least one uppercase letter\nAt least minimum 7 characters\nAt least maximum 12 Characters\nAn exclamation point: !");
+            password = Console.ReadLine();
+                // Setting up the first requirement
+                if (password)
+        }
+    }
+}
