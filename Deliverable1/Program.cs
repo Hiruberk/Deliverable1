4cf081854d281ab15b1104575d62cd2deae76e4b
Nate <nsdavis@mtu.edu>
2020-12-12 20:25:23 -0500

    shortened final deliverable 1

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -4,45 +4,36 @@ namespace Deliverable1
 {
     class Program
     {
-        static void Main(string[] args) { 
-        //Intializing the requirement check variables along with user input variable and the boolean condition
-        string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string lowercase = "abcdefghijklmnopqrstuvwxyz";
-        int minlength = 7;
-        int maxlength = 12;
-        string exclamation = "!";
-        string password;
-        bool valid = false;
-        //Intializes the loop to iterate until it meets requirements.
-            while(!valid)
+        static void Main(string[] args)
+        {
+            //Intializing the requirement check variables along with user input variable and the boolean condition
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            int minlength = 7;
+            int maxlength = 12;
+            string exclamation = "!";
+            string password;
+            bool valid = false;
+            //Intializes the loop to iterate until it meets requirements.
+            while (!valid)
             {
-            // Describes requirements and asks for input.
-            Console.WriteLine("Please enter a Password: " +
-                "" +
-                "The password must contain the following requirements:" +
-                "At least one lowercase letter" +
-                "At least one uppercase letter" +
-                "At least minimum 7 characters" +
-                "At least maximum 12 Characters" +
-                "An exclamation point: !");
-            password = Console.ReadLine();
-            // Setting up the first requirement, contains upper case
-            if (password.Contains(uppercase)) {;
-                // Setting up the second requirement, contains lower case
-                if (password.Contains(lowercase)) {;
-                    //Setting up the third requirement, min length
-                    if (password.Length >= minlength) {;
-                        //Setting up the fourth requirement, max length
-                        if (password.Length <= maxlength) {;
-                            //Setting up the fifth requirement, contains !
-                            if (password.Contains(exclamation)) {;
-                                valid = true; /* Breaks the While Loop allowing for accepted password message */
-                                //Output if error
-                            } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-                        } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-                    } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-                } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-            } else { Console.WriteLine("Invalid Password, Please Try Again"); }
+                // Describes requirements and asks for input.
+                Console.WriteLine("Please enter a Password: \n\n" +
+                    "The password must contain the following requirements:\n" +
+                    "At least one lowercase letter\n" +
+                    "At least one uppercase letter\n" +
+                    "At least minimum 7 characters\n" +
+                    "At least maximum 12 Characters\n" +
+                    "An exclamation point: !");
+
+                password = Console.ReadLine();
+                // Setting up the requirements
+                if (password.Contains(uppercase) && password.Contains(lowercase) && password.Contains(exclamation) && password.Length <= maxlength && password.Length >= minlength)
+                {
+                   valid = true; /* Breaks the While Loop allowing for accepted password message */
+                    //Output if error
+                }
+                else { Console.WriteLine("Error, Please Try Again"); }
 
 
             }
