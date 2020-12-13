898ca5df4a644293296fbfe2c50215a93071a734
Nate <nsdavis@mtu.edu>
2020-12-12 19:25:48 -0500

    Different Approach 1 Delv 1

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -17,32 +17,26 @@ namespace Deliverable1
             while(!valid)
             {
             // Describes requirements and asks for input.
-            Console.WriteLine("Please enter a Password: " +
-                "" +
-                "The password must contain the following requirements:" +
-                "At least one lowercase letter" +
-                "At least one uppercase letter" +
-                "At least minimum 7 characters" +
-                "At least maximum 12 Characters" +
+            Console.WriteLine("Please enter a Password: \n\n"+ 
+                
+                "The password must contain the following requirements:\n"+ 
+                "At least one lowercase letter.\n" + 
+                "At least one uppercase letter.\n" + 
+                "At least minimum 7 characters.\n" + 
+                "At least maximum 12 Characters.\n" + 
                 "An exclamation point: !");
             password = Console.ReadLine();
             // Setting up the first requirement, contains upper case
-            if (password.Contains(uppercase)) {;
-                // Setting up the second requirement, contains lower case
-                if (password.Contains(lowercase)) {;
-                    //Setting up the third requirement, min length
-                    if (password.Length >= minlength) {;
-                        //Setting up the fourth requirement, max length
-                        if (password.Length <= maxlength) {;
-                            //Setting up the fifth requirement, contains !
-                            if (password.Contains(exclamation)) {;
-                                valid = true; /* Breaks the While Loop allowing for accepted password message */
-                                //Output if error
+                if (password.Contains(uppercase) && password.Contains(lowercase) && password.Contains(exclamation)) {;
+                           //Setting up the fourth requirement, max length
+                            if (password.Length <= maxlength) {;
+                                //Setting up the fifth requirement, contains !
+                                if (password.Length >= minlength) {
+                                    valid = true; /* Breaks the While Loop allowing for accepted password message */
+                                    //Outputs for errors
+                                } else { Console.WriteLine("Invalid Password, Please Try Again"); }
                             } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-                        } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-                    } else { Console.WriteLine("Invalid Password, Please Try Again"); }
                 } else { Console.WriteLine("Invalid Password, Please Try Again"); }
-            } else { Console.WriteLine("Invalid Password, Please Try Again"); }
 
 
             }
